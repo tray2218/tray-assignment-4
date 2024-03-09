19e35a9d8715f1c3ca1784faebc1877e8b866a31
tray2218 <tray2218@gmail.com>
2024-03-09 13:06:54 -0500

    A4 changes

--- src/com/coderscampus/Assignment4Application.java
+++ src/com/coderscampus/Assignment4Application.java
@@ -3,7 +3,12 @@ package com.coderscampus;
 public class Assignment4Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// generate a random number
+		
+		// and collect user input
+		
+		// etc.
 
 	}
 
